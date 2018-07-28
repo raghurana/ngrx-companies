056f53383bc85a09fb9718741dacc9028b71af15
Raghu Rana <raghurana@gmail.com>
2018-07-28 10:24:34 +1000

    Deleted unecessary app files and modules.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  template: `<nav class="navbar navbar-default">
+              <a class="navbar-brand" [routerLink]="['/home']">ngrx companies</a>
+              <ul class="nav navbar-nav navbar-right">
+                <li class="nav-item" [routerLinkActive]="'active'">
+                  <a [routerLink]="['/company/list']">Companies</a>
+                </li>
+              </ul>
+            </nav>
+
+            <div class="container">
+              <router-outlet></router-outlet>
+            </div>`,
 })
 export class AppComponent {
-  title = 'ngrx-company';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { routes as AppRoutes } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,7 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(AppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
