d4689f321659404e263bbbf39833cddc2901e4f5
Raghu Rana <raghurana@gmail.com>
2018-07-28 17:27:18 +1000

    co-edit start

--- src/app/company/company.module.ts
+++ src/app/company/company.module.ts
@@ -4,13 +4,14 @@ import { RouterModule } from '@angular/router';
 import { CompanyListComponent } from './company-list/company-list.component';
 import { routes as CompanyRoutes } from './company.routes';
 import { CompanyListItemComponent } from './company-list-item/company-list-item.component';
+import { CompanyEditComponent } from './company-edit/company-edit.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(CompanyRoutes)
   ],
-  declarations: [CompanyListComponent, CompanyListItemComponent],
+  declarations: [CompanyListComponent, CompanyListItemComponent, CompanyEditComponent],
   providers: []
 })
 export class CompanyModule { }
--- src/app/company/company.routes.ts
+++ src/app/company/company.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CompanyListComponent } from './company-list/company-list.component';
+import { CompanyEditComponent } from './company-edit/company-edit.component';
 
 export const routes: Routes = [
-  { path: 'list', component: CompanyListComponent }
+  { path: 'list', component: CompanyListComponent },
+  { path: 'edit/:id', component: CompanyEditComponent }
 ];
